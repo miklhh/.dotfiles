4decf9312b02683cfa4971a841764f4c72eff1a7
Mikael Henriksson <mike.zx@hotmail.com>
2021-08-29 01:41:20 +0200

    Added LSP Hover, LSP Go to Definition etc to Vim config

---
+++ .config/nvim/lua/config/keybinds.lua
@@ -0,0 +1,28 @@
+---- Map tab to the above tab complete functions
+--vim.api.nvim_set_keymap('i', '<C-n>', 'v:lua.tab_complete()', { expr = true })
+--vim.api.nvim_set_keymap('s', '<C-n>', 'v:lua.tab_complete()', { expr = true })
+--vim.api.nvim_set_keymap('i', '<C-p>', 'v:lua.s_tab_complete()', { expr = true })
+--vim.api.nvim_set_keymap('s', '<C-p>', 'v:lua.s_tab_complete()', { expr = true })
+--
+---- Map compe confirm and complete functions
+--vim.api.nvim_set_keymap('i', 'ii', 'v:lua.on_ii()', { expr = true })
+--vim.api.nvim_set_keymap('i', '<c-space>', 'compe#complete()', { expr = true })
+--
+--vim.api.nvim_set_keymap('i', '<C-h>', 'v:lua.jump_prev()', { expr = true })
+--vim.api.nvim_set_keymap('s', '<C-h>', 'v:lua.jump_prev()', { expr = true })
+--vim.api.nvim_set_keymap('i', '<C-l>', 'v:lua.jump_next()', { expr = true })
+--vim.api.nvim_set_keymap('s', '<C-l>', 'v:lua.jump_next()', { expr = true })
+
+local function lsp_set_keymap(...) vim.api.nvim_set_keymap(...) end
+local lsp_opts =  { noremap=true, silent=true }
+-- See `:help vim.lsp.*` for documentation on any of the below functions
+lsp_set_keymap('n', '<space>i', '<cmd>lua vim.lsp.buf.hover()<CR>', lsp_opts)
+lsp_set_keymap('n', '<space>lw', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', lsp_opts)
+lsp_set_keymap('n', '<space>lgd', '<cmd>lua vim.lsp.buf.definition()<CR>', lsp_opts)
+lsp_set_keymap('n', '<space>lr', '<cmd>lua vim.lsp.buf.rename()<CR>', lsp_opts)
+lsp_set_keymap('n', '<space>lj', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', lsp_opts)
+--lsp_set_keymap('n', '<space>la', '<cmd>lua vim.lsp.buf.code_action()<CR>', lsp_opts)
+--lsp_set_keymap('n', '<space>lgi', '<cmd>lua vim.lsp.buf.implementation()<CR>', lsp_opts)
+--lsp_set_keymap('n', '<space>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', lsp_opts)
+--lsp_set_keymap('n', '<space>lJ', '<cmd>Telescope lsp_workspace_symbols<CR>', lsp_opts)
+--lsp_set_keymap('i', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', lsp_opts)
--- .vimrc
+++ .vimrc
@@ -63,6 +63,8 @@ call plug#begin('~/.vim/plugged')
     if has('nvim')
         Plug 'neovim/nvim-lspconfig'
         Plug 'hrsh7th/nvim-compe'
+        Plug 'hrsh7th/vim-vsnip'
+        Plug 'hrsh7th/vim-vsnip-integ'
     endif
 
     " NeoVim sudo read/write (:SudaRead, :SudaWrite)
@@ -213,33 +215,11 @@ endif
 " -----------------------------------------------------------------------------
 "
 if has('nvim')
-    " Settings for nvim-compe
-    set completeopt=menuone,noselect
-    let g:compe = {}
-    let g:compe.enabled = v:true
-    let g:compe.autocomplete = v:true
-    let g:compe.debug = v:false
-    let g:compe.min_length = 1
-    let g:compe.preselect = 'enable'
-    let g:compe.throttle_time = 80
-    let g:compe.source_timeout = 200
-    let g:compe.resolve_timeout = 800
-    let g:compe.incomplete_delay = 400
-    let g:compe.max_abbr_width = 100
-    let g:compe.max_kind_width = 100
-    let g:compe.max_menu_width = 100
-    let g:compe.documentation = v:true
 
-    let g:compe.source = {}
-    let g:compe.source.path = v:true
-    let g:compe.source.buffer = v:true
-    let g:compe.source.calc = v:true
-    let g:compe.source.nvim_lsp = v:true
-    let g:compe.source.nvim_lua = v:true
-    let g:compe.source.vsnip = v:true
-    let g:compe.source.ultisnips = v:true
-    let g:compe.source.luasnip = v:true
-    let g:compe.source.emoji = v:true
+lua << EOF
+    require('config/compe')
+    require('config/keybinds')
+EOF
 
     " -----------------------------------------------
     " --      C++ Language server with Clangd      --
