0c3d10dc190f5182ae4edff3d47e4cee18eabaa3
Mikael Henriksson <mike.zx@hotmail.com>
2022-10-20 13:38:53 +0200

    lsp.lua: update_capabilites(...) --> default_capabilites()

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -32,7 +32,7 @@ local server_name = 'vhdl_tool'
 local configs = require 'lspconfig.configs'
 if configs[server_name] == nil then
     local util = require 'lspconfig.util'
-    local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
     configs[server_name] = {
         default_config = {
             cmd = {'vhdl-tool', 'lsp'};
