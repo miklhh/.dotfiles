2e711cef42a2260813275beb38f10cbdb63da367
Mikael Henriksson <mikael.henriksson@liu.se>
2024-05-16 10:23:56 +0200

    ubuntu-22.04.bash: purge NodeJS

--- bootstrap/ubuntu-22.04.bash
+++ bootstrap/ubuntu-22.04.bash
@@ -84,3 +84,17 @@ apt-get -y install              \
     xdg-desktop-portal          \
     xdg-desktop-portal-gnome    \
     xdg-desktop-portal-gtk
+
+# Purge NodeJS
+apt-get -y remove nodejs npm
+rm -rf "${SUDO_USER_HOME}/.npm"
+rm -rf "${SUDO_USER_HOME}/.node-gyp"
+rm -rf /usr/local/bin/npm 
+rm -rf /usr/local/share/man/man1/node* 
+rm -rf /usr/local/lib/dtrace/node.d
+rm -rf /opt/local/bin/node
+rm -rf /opt/local/include/node
+rm -rf /opt/local/lib/node_modules
+rm -rf /usr/local/lib/node*
+rm -rf /usr/local/include/node*
+rm -rf /usr/local/bin/node*
