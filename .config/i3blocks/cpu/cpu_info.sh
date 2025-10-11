0e58ba4659e17837505bd6ad74111163677bff1a
Mikael Henriksson <mikael.henriksson@liu.se>
2025-10-11 15:32:24 +0200

    misc: remove unused configs

--- .config/i3blocks/cpu/cpu_info.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-TEMP=$(sensors | awk '/Package id 0:/{ printf("%s\n", $4) }')
-CPU_USAGE=$(mpstat 1 1 | awk '/Average:/ {printf("%s\n", $(NF-9))}')
-echo "$CPU_USAGE $TEMP" | awk '{ printf(" CPU:%6s% @ %s \n"), $1, $2 }'
