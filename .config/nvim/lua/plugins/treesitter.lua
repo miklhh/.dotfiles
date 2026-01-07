feda162013edf0fbec04b4e09e0a93140703e75c
Mikael Henriksson <mikael.henriksson@liu.se>
2026-01-07 17:32:53 +0100

    init.vim: update treesitter to main

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,56 +2,8 @@
 
 return {
     {
-        "nvim-treesitter/nvim-treesitter",
-        version = false,
-        build = ":TSUpdate",
-        event = { "VeryLazy" },
-        lazy = vim.fn.argc(-1) == 0, -- load treesitter early if opening file on cmdline
-        init = function(plugin)
-            -- Some lazy performance optimization.
-            -- See: `http://www.lazyvim.org/plugins/treesitter` for more info
-            require("lazy.core.loader").add_to_rtp(plugin)
-            require("nvim-treesitter.query_predicates")
-        end,
-        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-        config = function()
-            local configs = require("nvim-treesitter.configs")
-            configs.setup({
-                highlight = { enable = true },
-                indent = { enable = true },
-                sync_install = false,
-                ensure_installed = {
-                    "bash",
-                    "c",
-                    "cmake",
-                    "cpp",
-                    "dockerfile",
-                    "dot",
-                    "go",
-                    "javascript",
-                    "julia",
-                    "llvm",
-                    "lua",
-                    "make",
-                    "markdown",
-                    "markdown_inline",
-                    "matlab",
-                    "ninja",
-                    "passwd",
-                    "perl",
-                    "python",
-                    "query",
-                    "regex",
-                    "ruby",
-                    "rust",
-                    "toml",
-                    "typescript",
-                    "vhdl",
-                    "vim",
-                    "vimdoc",
-                    "yaml",
-                },
-            })
-        end,
-    },
+      "nvim-treesitter/nvim-treesitter",
+      lazy = false,
+      build = ":TSUpdate"
+    }
 }
